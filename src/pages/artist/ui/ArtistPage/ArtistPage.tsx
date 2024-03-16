6cb15e1db2d333175a72d25306ae6001c31f61e2
Major Boner <khodukin88@gmail.com>
2024-03-16 22:51:44 +0700

    upd artist page header

--- src/pages/artist/ui/ArtistPage/ArtistPage.tsx
+++ src/pages/artist/ui/ArtistPage/ArtistPage.tsx
@@ -11,8 +11,13 @@ export const ArtistPage = memo(function ArtistPage(props: ArtistPageProps) {
   const { className } = props;
   return (
     <div className={classNames(cls.ArtistPage, {}, [className])}>
-      <ArtistBanner src="https://roco.studio/wp-content/uploads/2014/12/img_banner-thin_mountains.jpg" />
-      <ArtistAvatar src="https://as2.ftcdn.net/v2/jpg/01/74/35/41/1000_F_174354171_dxcNdq4XMmARKT7E88NOLmdrv6BBSAww.jpg" />
+      <div className={cls.header}>
+        <ArtistBanner src="https://roco.studio/wp-content/uploads/2014/12/img_banner-thin_mountains.jpg" />
+        <ArtistAvatar
+          src="https://as2.ftcdn.net/v2/jpg/01/74/35/41/1000_F_174354171_dxcNdq4XMmARKT7E88NOLmdrv6BBSAww.jpg"
+          className={cls.avatar}
+        />
+      </div>
     </div>
   );
 });
